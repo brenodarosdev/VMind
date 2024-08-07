f45bd37e262cd41a9c5490a6a94a4d005a7a3d97
Breno Darós <brenodarosdev@gmail.com>
2024-08-07 18:59:15 -0300

    initial commit

---
+++ src/main/java/com/vmind/virtual_assistants/VmindApplication.java
@@ -0,0 +1,13 @@
+package com.vmind.virtual_assistants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VmindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VmindApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vmind/virtual_assistants/VmindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vmind.virtual_assistants;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VmindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
