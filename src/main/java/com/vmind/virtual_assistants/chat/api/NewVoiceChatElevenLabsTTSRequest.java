6227ca598f472596dbb6449e9791be2160e01873
Breno Darós <brenodarosdev@gmail.com>
2024-08-08 19:37:26 -0300

    obtem newVoiceChatResponse

--- src/main/java/com/vmind/virtual_assistants/chat/api/NewVoiceChatElevenLabsTTSRequest.java
+++ src/main/java/com/vmind/virtual_assistants/chat/api/NewVoiceChatElevenLabsTTSRequest.java
@@ -1,7 +1,6 @@
 package com.vmind.virtual_assistants.chat.api;
 
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotNull;
 import lombok.Value;
 
 @Value
--- src/main/java/com/vmind/virtual_assistants/chat/service/ChatApplicationService.java
+++ src/main/java/com/vmind/virtual_assistants/chat/service/ChatApplicationService.java
@@ -1,17 +1,28 @@
 package com.vmind.virtual_assistants.chat.service;
 
+import com.vmind.virtual_assistants.chat.api.NewVoiceChatElevenLabsTTSRequest;
 import com.vmind.virtual_assistants.chat.api.NewVoiceChatRequest;
 import com.vmind.virtual_assistants.chat.api.NewVoiceChatResponse;
+import com.vmind.virtual_assistants.openai.service.OpenaiService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.stereotype.Service;
 
 @Log4j2
 @Service
+@RequiredArgsConstructor
 public class ChatApplicationService implements ChatService {
+    private final OpenaiService openaiService;
+
     @Override
     public NewVoiceChatResponse newVoiceChat(NewVoiceChatRequest voiceChatRequest) {
         log.debug("[start] ChatApplicationService - newVoiceChat");
+        NewVoiceChatElevenLabsTTSRequest VoiceChatElevenLabsTTS = (NewVoiceChatElevenLabsTTSRequest) voiceChatRequest;
+        ChatResponse openaiResponse = openaiService.callChatModel(VoiceChatElevenLabsTTS.getOpenaiCallRequest());
+        NewVoiceChatResponse response = new NewVoiceChatResponse(openaiResponse.getResult().getOutput().getMetadata(),
+                openaiResponse.getResult().getOutput().getContent());
         log.debug("[finish] ChatApplicationService - newVoiceChat");
-        return null;
+        return response;
     }
 }
