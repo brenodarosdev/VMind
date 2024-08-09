70341cb7de634ef0c71f14c4cf9eaebe98c3337f
Breno Darós <brenodarosdev@gmail.com>
2024-08-09 14:44:04 -0300

    implementa DTO e bodyJsonString

--- src/main/java/com/vmind/virtual_assistants/elevenlabs/service/ElevenLabsApplicationService.java
+++ src/main/java/com/vmind/virtual_assistants/elevenlabs/service/ElevenLabsApplicationService.java
@@ -1,5 +1,6 @@
 package com.vmind.virtual_assistants.elevenlabs.service;
 
+import com.google.gson.Gson;
 import com.vmind.virtual_assistants.chat.application.api.ElevenLabsTTSRequest;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
@@ -10,6 +11,8 @@ public class ElevenLabsApplicationService implements ElevenLabsService {
     @Override
     public void textToSpeech(String content, ElevenLabsTTSRequest request) {
         log.debug("[start] ElevenLabsApplicationService - textToSpeech");
+        String bodyJsonString = new Gson()
+                .toJson(new ElevenLabsTTSRequestDTO(content, request));
         log.debug("[finish] ElevenLabsApplicationService - textToSpeech");
     }
 }
