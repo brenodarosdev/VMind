a3d2e0cc6b2506974e55d4b0f9f468ca84dec5a6
Breno Darós <brenodarosdev@gmail.com>
2024-09-16 18:42:57 -0300

    remove modelId de ElevenLabsTTSSettings e add valor fixo no DTO

--- src/main/java/com/vmind/virtual_assistants/elevenlabs/application/service/ElevenLabsTTSRequestDTO.java
+++ src/main/java/com/vmind/virtual_assistants/elevenlabs/application/service/ElevenLabsTTSRequestDTO.java
@@ -4,13 +4,12 @@ import com.vmind.virtual_assistants.chat.application.api.ElevenLabsTTSSettings;
 
 public class ElevenLabsTTSRequestDTO {
     private String text;
-    private String model_id;
+    private String model_id = "eleven_turbo_v2_5";
     private String language_code;
     private VoiceSettingsDTO voice_settings;
 
     public ElevenLabsTTSRequestDTO(String input, ElevenLabsTTSSettings request) {
         this.text = input;
-        this.model_id = request.getModelId();
         this.language_code = request.getLanguageCode();
         this.voice_settings = new VoiceSettingsDTO(request);
     }
