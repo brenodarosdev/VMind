1cdba22d10a5e0ec97c841fa643a6f5413f5c743
Breno Darós <brenodarosdev@gmail.com>
2024-09-17 11:44:41 -0300

    ajusta tts dto de elevenlabs

--- src/main/java/com/vmind/virtual_assistants/elevenlabs/application/service/ElevenLabsTTSRequestDTO.java
+++ src/main/java/com/vmind/virtual_assistants/elevenlabs/application/service/ElevenLabsTTSRequestDTO.java
@@ -4,12 +4,13 @@ import com.vmind.virtual_assistants.chat.application.api.ElevenLabsTTSSettings;
 
 public class ElevenLabsTTSRequestDTO {
     private String text;
-    private String model_id = "eleven_turbo_v2_5";
+    private String model_id;
     private String language_code;
     private VoiceSettingsDTO voice_settings;
 
-    public ElevenLabsTTSRequestDTO(String input, ElevenLabsTTSSettings request) {
+    public ElevenLabsTTSRequestDTO(String input, String modelId, ElevenLabsTTSSettings request) {
         this.text = input;
+        this.model_id = modelId;
         this.language_code = request.getLanguageCode();
         this.voice_settings = new VoiceSettingsDTO(request);
     }
